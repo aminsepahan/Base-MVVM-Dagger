993dd8458fa1f88209c436cc6ed0215d8fb53f6e
aminkeshavarzian <aminkeshavarzian@gmail.com>
2019-06-16 10:44:56 +0430

    removed unnecessary and build folders from git

---
+++ app/src/main/kotlin/com/amin/homitel/ui/detail/DetailsFragment.kt
@@ -0,0 +1,23 @@
+package com.amin.homitel.ui.detail
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amin.homitel.R
+
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+
+}
